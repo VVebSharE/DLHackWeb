358910400b82a77cec1771b308c6eb5e7a6d0c49
VVebSharE <78077715+VVebSharE@users.noreply.github.com>
2024-04-25 10:25:06 +0530

    changes

--- inference.py
+++ inference.py
@@ -3,7 +3,6 @@ from database import db
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 import uuid
 import os
-import cv2
 import numpy as np
 import base64
 
@@ -13,20 +12,6 @@ def model(img):
 
 infer = Blueprint('infer', __name__)
 
-
-def dummy_segmentation(image_data):
-    # Convert image data to numpy array
-    nparr = np.frombuffer(image_data, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # Dummy segmentation (convert to grayscale)
-    gray_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-
-    # Convert back to bytes
-    _, segmented_img_data = cv2.imencode(".png", gray_img)
-    segmented_img_bytes = segmented_img_data.tobytes()
-
-    return segmented_img_bytes
 
 
 @infer.route('/segmentImage', methods=['POST']) #save image if user is logged in other no
